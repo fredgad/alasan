2b71eaf68b3a8045c44b4c1dd34b8bb11e94f9a8
fredgad <31152572+fredgad@users.noreply.github.com>
2019-10-02 18:07:51 +0300

    f

---
+++ app/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$(function () {// window.addEventListener('scroll', ()=> {
+  //     if($(window).width() < 910) {
+  //         let scrolled = window.pageYOffset || document.scrollTop;
+  //         if (scrolled > 150) {
+  //         } else {
+  //         }
+  //    }
+  // });
+  // $('.nav-bar').click((e) => {
+  //     $('.nav-bar').toggleClass('active');
+  //     $('.nav-cont').toggleClass('down');
+  //     $('.nav-cont').css('top', $('.nav-bar').offset().top + 29 + 'px');
+  // });
+});
